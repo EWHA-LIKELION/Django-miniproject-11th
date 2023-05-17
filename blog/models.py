edac636aff84f19b1237373a821ad487ef8d1541
ehyeji <jiyaaaa22@gamil.com>
2023-05-18 04:21:29 +0900

    add comment, hashtag function

--- blog/models.py
+++ blog/models.py
@@ -1,6 +1,14 @@
 from django.db import models
+from django.utils import timezone
 
 # Create your models here.
+
+
+class HashTag(models.Model):
+    hashtag=models.CharField(max_length=100)
+
+    def __str__(self):
+        return self.hashtag
 
 
 
@@ -9,10 +17,22 @@ class Blog(models.Model):
     author=models.CharField(max_length=50)
     date=models.DateTimeField('date published')
     body=models.TextField()
+    hashtag=models.ManyToManyField(HashTag)
 
     def __str__(self):
         return self.title
     
     def summary(self):
         return self.body[:35]
+    
+class Comment(models.Model):
+    post = models.ForeignKey(Blog, related_name='comments', on_delete=models.CASCADE)
+    username = models.CharField(max_length=20)
+    comment_text = models.TextField()
+    created_at = models.DateTimeField(default=timezone.now)
 
+    def approve(self):
+        self.save()
+
+    def __str__(self):
+        return self.comment_text
