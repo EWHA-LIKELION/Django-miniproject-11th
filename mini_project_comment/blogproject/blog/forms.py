6cc0a5b18a24ddd5a8bb078d4d63465a564e2f9c
yewon <shimyewon@ewhain.net>
2023-05-18 12:33:00 +0900

    심예원:230518

---
+++ mini_project_comment/blogproject/blog/forms.py
@@ -0,0 +1,13 @@
+from django import forms
+from .models import Blog, Comment, HashTag
+
+class Blogform(forms.ModelForm):
+    class Meta:
+        model = Blog
+        fields = ['title','body']
+
+
+class Commentform(forms.ModelForm):
+    class Meta:
+        model = Comment
+        fields = ['comment']
